9d52fa3480eb92da98b8b80c558cde2182282b23
Mefistofel <mfstfl@gmail.com>
2023-12-06 21:12:48 +0100

    Fixed icons

--- Game15Project/app/build.gradle.kts
+++ Game15Project/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "indie.mefistofel.game15"
         minSdk = 21
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -53,7 +53,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.activity:activity-compose:1.8.1")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
